c6011f77a8190c51e2734bfa98fb73d1d8037c2e
gandatadyo <gandatadyosurya@yahoo.com>
2021-07-12 09:40:07 +0700

    update payment gojek

--- router_system.js
+++ router_system.js
@@ -30,7 +30,7 @@ exports.sys_api = async (req, res) => {
                 }
             } break;
             case 'create_billing': {
-                const { kindpayemnt, grandtotal } = req.body
+                const { idpayment, grandtotal } = req.body
                 // Generate No. Invoice
                 const nanoid = customAlphabet('1234567890', 10)
                 const docnumber = `test-${nanoid()}`
@@ -39,75 +39,109 @@ exports.sys_api = async (req, res) => {
                 // Insert Data Billing To Database 
                 const ssqlinsertBilling = `INSERT INTO billing_midtrans (DocNumber,GrandTotal,StatusPayment,TimeCreated,TimeUpdated) VALUES ("${docnumber}","${grandtotal}","none",NOW(),NOW());`
                 const dataResolve = await router_module.ExecQueryV1(ssqlinsertBilling)
-                // Paramenter For Payment Midtrans
-                let parameter = {
-                    "payment_type": "bank_transfer",
-                    "transaction_details": {
-                        "gross_amount": grandtotal,
-                        "order_id": docnumber,
-                    },
-                    "bank_transfer": {
-                        "bank": kindpayemnt
-                    }
-                };
-
-                // Cretae Billing On Midtrans
-                console.log('run core api');
-                coreApi.charge(parameter)
-                    .then((chargeResponse) => {
-                        console.log('chargeResponse:', JSON.stringify(chargeResponse));
-                        responseCoreApi(chargeResponse)
-                    })
-                    .catch((e) => {
-                        console.log('Error occured:', e.message);
-                        res.send({ status: 'false', message: e.message })
-                    });
-
-                // function 
-                async function responseCoreApi(statusResponse) {
-                    let orderId = statusResponse.order_id;
-                    let transactionStatus = statusResponse.transaction_status;
-                    let fraudStatus = statusResponse.fraud_status;
-
-                    console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
-
-                    // Sample transactionStatus handling logic
-                    let ssqlUpdateBilling = ''
-                    if (transactionStatus == 'capture') {
-                        // capture only applies to card transaction, which you need to check for the fraudStatus
-                        if (fraudStatus == 'challenge') {
-                            // TODO set transaction status on your databaase to 'challenge'
-                            ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="challenge" WHERE DocNumber="${docnumber}"`
-                        } else if (fraudStatus == 'accept') {
-                            // TODO set transaction status on your databaase to 'success'
-                            ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="success" WHERE DocNumber="${docnumber}"`
+                // get data payment
+                const dataPaymentMethod = await router_module.OpenQueryV1(`SELECT CodePayment,NamePayment,TypePayment FROM paymentmethod_midtrans WHERE ID="${idpayment}"`)
+                if (dataPaymentMethod.dataset.length > 0) {
+                    const dataUpdateMethodpayment = await router_module.ExecQueryV1(`UPDATE billing_midtrans SET PaymentMethod="${dataPaymentMethod.dataset[0].CodePayment}" WHERE DocNumber="${docnumber}"`)
+                    let parameter = {
+                        "transaction_details": {
+                            "gross_amount": grandtotal,
+                            "order_id": docnumber,
                         }
-                    } else if (transactionStatus == 'settlement') {
-                        // TODO set transaction status on your databaase to 'success'
-                        ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
-                    } else if (transactionStatus == 'deny') {
-                        // TODO you can ignore 'deny', because most of the time it allows payment retries
-                        // and later can become success
-                        ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
-                    } else if (transactionStatus == 'cancel' ||
-                        transactionStatus == 'expire') {
-                        // TODO set transaction status on your databaase to 'failure'
-                        ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
-                    } else if (transactionStatus == 'pending') {
-                        // TODO set transaction status on your databaase to 'pending' / waiting payment
-                        ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
                     }
-                    if (ssqlUpdateBilling != '') {
-                        const dataUpdateBillingResolve = await router_module.ExecQueryV1(ssqlUpdateBilling)
+                    if (dataPaymentMethod.dataset[0].TypePayment == "Bank Transfer") {
+                        // Paramenter For Payment Midtrans
+                        parameter.payment_type = "bank_transfer"
+                        parameter.bank_transfer = {
+                            "bank": dataPaymentMethod.dataset[0].CodePayment
+                        }
+                    } else if (dataPaymentMethod.dataset[0].TypePayment == "E-Money" && dataPaymentMethod.dataset[0].CodePayment == "gopay") {
+                        parameter.payment_type = "gopay"
+                        parameter.gopay = {
+                            "enable_callback": true,                // optional
+                            "callback_url": "someapps://callback"   // optional
+                        }
                     }
 
-                    res.send({ status: 'true', message: 'Billing Created', orderId: orderId, transactionStatus: transactionStatus, fraudStatus: fraudStatus })
+                    // Cretae Billing On Midtrans
+                    console.log('run core api');
+                    coreApi.charge(parameter)
+                        .then((chargeResponse) => {
+                            console.log('chargeResponse:', JSON.stringify(chargeResponse));
+                            responseCoreApi(chargeResponse)
+                        })
+                        .catch((e) => {
+                            console.log('Error occured:', e.message);
+                            res.send({ status: 'false', message: e.message })
+                        });
+
+                    // function 
+                    async function responseCoreApi(statusResponse) {
+                        let orderId = statusResponse.order_id;
+                        let transactionStatus = statusResponse.transaction_status;
+                        let fraudStatus = statusResponse.fraud_status;
+
+                        console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
+
+                        // Sample transactionStatus handling logic
+                        let ssqlUpdateBilling = ''
+                        if (transactionStatus == 'capture') {
+                            // capture only applies to card transaction, which you need to check for the fraudStatus
+                            if (fraudStatus == 'challenge') {
+                                // TODO set transaction status on your databaase to 'challenge'
+                                ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="challenge" WHERE DocNumber="${docnumber}"`
+                            } else if (fraudStatus == 'accept') {
+                                // TODO set transaction status on your databaase to 'success'
+                                ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="success" WHERE DocNumber="${docnumber}"`
+                            }
+                        } else if (transactionStatus == 'settlement') {
+                            // TODO set transaction status on your databaase to 'success'
+                            ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
+                        } else if (transactionStatus == 'deny') {
+                            // TODO you can ignore 'deny', because most of the time it allows payment retries
+                            // and later can become success
+                            ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
+                        } else if (transactionStatus == 'cancel' ||
+                            transactionStatus == 'expire') {
+                            // TODO set transaction status on your databaase to 'failure'
+                            ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
+                        } else if (transactionStatus == 'pending') {
+                            // TODO set transaction status on your databaase to 'pending' / waiting payment
+                            ssqlUpdateBilling = `UPDATE billing_midtrans SET StatusPayment="${transactionStatus}" WHERE DocNumber="${docnumber}"`
+                        }
+                        if (ssqlUpdateBilling != '') {
+                            const dataUpdateBillingResolve = await router_module.ExecQueryV1(ssqlUpdateBilling)
+                        }
+
+                        let paymentInfo = ''
+                        if (statusResponse.payment_type == 'bank_transfer') {
+                            if (statusResponse.va_numbers.length > 0) {
+                                paymentInfo = {
+                                    payment_type: 'bank transfer',
+                                    bank: statusResponse.va_numbers[0].bank,
+                                    va_number: statusResponse.va_numbers[0].va_number,
+                                }
+                            }
+                        } else if (statusResponse.payment_type == 'gopay') {
+                            const dataUpdatNotesPayment = await router_module.ExecQueryV1(`UPDATE billing_midtrans SET NOTES='${JSON.stringify(statusResponse.actions)}' WHERE DocNumber="${docnumber}"`)
+                            const dataNotesPayment = await router_module.OpenQueryV1(`SELECT Notes FROM billing_midtrans WHERE DocNumber="${docnumber}"`)
+                            const dataNotes = JSON.parse(dataNotesPayment.dataset[0].Notes)
+                            paymentInfo = {
+                                payment_type: 'gopay',
+                                bank: dataNotes[1].url,
+                                va_number: dataNotes[0].url,
+                            }
+                        }
+                        res.send({ status: 'true', message: 'Billing Created', orderId: orderId, transactionStatus: transactionStatus, fraudStatus: fraudStatus, paymentInfo: paymentInfo })
+                    }
+                } else {
+                    res.send({ status: 'false', message: 'Data pembayaran tidak ditemukan' })
                 }
             } break;
             case 'status_billing': {
                 const { docnumber } = req.body
                 coreApi.transaction.status(docnumber)
-                    .then((statusResponse) => {
+                    .then(async (statusResponse) => {
                         console.log(statusResponse);
                         let orderId = statusResponse.order_id;
                         let transactionStatus = statusResponse.transaction_status;
@@ -122,6 +156,14 @@ exports.sys_api = async (req, res) => {
                                     bank: statusResponse.va_numbers[0].bank,
                                     va_number: statusResponse.va_numbers[0].va_number,
                                 }
+                            }
+                        } else if (statusResponse.payment_type == 'gopay') {
+                            const dataNotesPayment = await router_module.OpenQueryV1(`SELECT Notes FROM billing_midtrans WHERE DocNumber="${docnumber}"`)
+                            const dataNotes = JSON.parse(dataNotesPayment.dataset[0].Notes)
+                            paymentInfo = {
+                                payment_type: 'gopay',
+                                bank: dataNotes[1].url,
+                                va_number: dataNotes[0].url,
                             }
                         }
                         res.send({ status: 'true', message: message, orderId: orderId, transactionStatus: transactionStatus, fraudStatus: fraudStatus, paymentInfo: paymentInfo })
